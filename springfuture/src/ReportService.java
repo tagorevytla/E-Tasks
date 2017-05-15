bbba7ecd4758aa5530527cb8589505c00971e3fa
vytlasai <opusone222@gmail.com>
2017-05-15 13:00:34 -0500

    async project running

---
+++ springfuture/src/ReportService.java
@@ -0,0 +1,7 @@
+/**
+ * Created by Vytlasai on 5/15/2017.
+ */
+public interface ReportService {
+
+    public Future<report> generateReport();
+}
---
+++ springfuture/src/ReportServiceImpl.java
@@ -0,0 +1,10 @@
+/**
+ * Created by Vytlasai on 5/15/2017.
+ */
+public class ReportServiceImpl implements ReportService {
+
+    @Async
+    public Future<report> generateReport() {
+        return null;
+    }
+}
